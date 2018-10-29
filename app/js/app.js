981789ee2641d23692fcfccc1c833bd39151c991
Javier <jmartin@grupoloyal.com>
2018-10-29 23:33:37 +0100

    Escenario

--- app/js/app.js
+++ app/js/app.js
@@ -31,3 +31,80 @@ angular.module("MyApp").factory("auth", [
     };
   }
 ]);
+
+
+angular.module('MyApp').controller('NotificationController', ['NotificationService', function (srv) {
+  this.notify = srv;
+}]);
+angular.module('MyApp').factory('NotificationService', ['$log', function ($log) {
+  return {
+      listado: [],
+      add: function (msg) {
+          if (msg) {
+              this.listado.push(msg);
+          } else {
+              $log.error('Falta el mensaje.');
+          }
+          this.hayMensajes = this.listado.length > 0;
+      },
+      remove: function (index) {
+          if (0 <= index && index < this.listado.length) {
+              this.listado.splice(index, 1);
+          } else {
+              $log.error('Index out of range.');
+          }
+          this.hayMensajes = this.listado.length > 0;
+      },
+      clear: function () {
+          this.listado = [];
+          this.hayMensajes = false;
+      },
+      hayMensajes: false,
+  };
+}]);
+
+angular.module('MyApp').factory('AuthService', ['$log', function ($log) {
+  var isAuth = true;
+  var authToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjoiYWRtaW4iLCJleHBpcmVzSW4iOiIxaCIsImlhdCI6MTU0MDM5NTc3NH0.aa30kqDjeRzIwV5tPVvR5gMGltFebzAbIpGe693MFnI';
+  var name = 'admin';
+
+  if (localStorage && localStorage.AuthService) {
+      rslt = JSON.parse(localStorage.AuthService);
+      isAuth = rslt.isAuth;
+      authToken = rslt.authToken;
+      name = rslt.name;
+  }
+
+  return {
+      AuthorizationHeader: function () {
+          return authToken;
+      },
+      isAutenticated: function () {
+          return isAuth;
+      },
+      Name: function () {
+          return name;
+      },
+      login: function (token, nombre) {
+          isAuth = true;
+          authToken = token;
+          name = nombre;
+          if (localStorage) {
+              localStorage.AuthService = JSON.stringify({
+                  isAuth: isAuth,
+                  authToken: authToken,
+                  name: name
+              });
+          }
+      },
+      logout: function () {
+          isAuth = false;
+          authToken = '';
+          name = '';
+          if (localStorage) {
+              localStorage.removeItem('AuthService');
+          }
+      },
+  };
+}]);
+
---
+++ app/js/personas.js
@@ -0,0 +1,101 @@
+﻿angular.module('MyApp').controller('PersonasController',
+    ['NotificationService', '$window', '$route', '$routeParams', '$location', 'PersonasService',
+    function (notify, $window, $route, $routeParams, $location, srv) {
+        var vm = this;
+
+        vm.modo = 'list';
+        vm.listado = srv.listado;
+        vm.elemento = {};
+        var idOriginal = null;
+        var urlList = '/personas';
+        var pk = 'id';
+
+        vm.list = function () {
+            vm.modo = 'list';
+        };
+
+        vm.add = function () {
+            vm.elemento = {};
+            vm.modo = 'add';
+        };
+        vm.edit = function (key) {
+            var rslt = vm.listado.find(function (item) { return item[pk] == key; });
+            if (rslt) {
+                vm.elemento = angular.copy(rslt);
+                idOriginal = key;
+                vm.modo = 'edit';
+            } else {
+                notify.add('Elemento no encontrado.');
+            }
+        };
+        vm.view = function (key) {
+            var rslt = vm.listado.find(function (item) { return item[pk] == key; });
+            if (rslt) {
+                vm.elemento = angular.copy(rslt);
+                vm.modo = 'view';
+            } else {
+                notify.add('Elemento no encontrado.');
+            }
+        };
+        vm.delete = function (key) {
+            if (!$window.confirm('¿Seguro?')) return;
+            var index = vm.listado.findIndex(function (item) { return item[pk] == key; });
+            if (index >= 0) {
+                vm.listado.splice(index, 1);
+                vm.list();
+            } else {
+                notify.add('Elemento no encontrado.');
+            }
+        };
+
+        vm.cancel = function () {
+            vm.elemento = {};
+            idOriginal = null;
+            $location.url(urlList);
+        };
+        vm.send = function () {
+            switch (vm.modo) {
+                case 'add':
+                    vm.listado.push(vm.elemento);
+                    vm.cancel();
+                    break;
+                case 'edit':
+                    var index = vm.listado.findIndex(function (item) { return item[pk] == idOriginal; });
+                    if (index >= 0) {
+                        vm.listado[index] = vm.elemento;
+                        vm.list();
+                    } else {
+                        notify.add('Elemento no encontrado.');
+                    }
+                    vm.cancel();
+                    break;
+                case 'view':
+                    vm.cancel();
+                    break;
+            }
+        };
+
+        if ($routeParams.id) {
+            if ($location.url().endsWith('/edit')) {
+                vm.edit($routeParams.id);
+            } else {
+                vm.view($routeParams.id);
+            }
+        } else {
+            if ($location.url().endsWith('/add')) {
+                vm.add();
+            } else {
+                vm.list();
+            }
+        }
+}]);
+angular.module('MyApp').factory('PersonasService',
+    [function () {
+        return {
+            listado : [
+            { id: 1, nombre: 'Carmelo', apellidos: 'Coton', edad: 34 },
+            { id: 2, nombre: 'Pepito', apellidos: 'Grillo', edad: 155 },
+            { id: 3, nombre: 'Pedro', apellidos: 'Pica Piedra', edad: 50 },
+            { id: 4, nombre: 'Pablo', apellidos: 'Marmol', edad: 47 },
+        ]};
+}]);
